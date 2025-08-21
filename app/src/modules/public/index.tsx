6335b12a021f110487daa36310ae8b2baff920fe
Lennert Van Geert <lennert.vangeert@gmail.com>
2025-08-21 23:45:16 +0200

    fix: update last modified dates in sitemap.xml and comment out unused ContactPage route

--- app/src/modules/public/index.tsx
+++ app/src/modules/public/index.tsx
@@ -1,14 +1,13 @@
 import { RouteObject } from "react-router-dom";
 import PageWrapper from "../sections/pageWrapper";
 import ChatPage from "./chatPage";
-import ContactPage from "./contactPage";
 import ProjectListPage from "./projectListPage";
 import IndexPage from "./indexPage";
 
 const subPages: RouteObject[] = [
   {
-    path: "contact",
-    element: <ContactPage />,
+    // path: "contact",
+    // element: <ContactPage />,
   },
 ];
 
